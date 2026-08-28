04d8cc37517afb45fb526d35d5decafe2cc9a7a4
Місяць <120984956+Mycetik@users.noreply.github.com>
2026-08-29 02:17:12 +0300

    main.cpp

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,1 +1,28 @@
+#include <MoonOS.h>
+#include <version.h>
 
+extern HardwareSerial Serial; 
+
+int main(void)
+{
+    init();
+    
+#if defined(USBCON)
+    USBDevice.attach();
+#endif
+
+    Serial.begin(115200);
+    Serial.println("Arduino success init");
+    Serial.print(2048-getFreeRam());
+    Serial.print("/2048 mem\r\n");
+    os_init();
+    Serial.print("Startup MoonOS Arduino ");
+    Serial.print(release);
+    Serial.print("...\r\n");
+
+    setup();
+
+    os_start();
+
+    return 0;
+}
